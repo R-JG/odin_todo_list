c777f2c4fcc9acd36bf91749c5feda07537b098b
Robin <robin.james.guerrier@gmail.com>
2022-10-01 16:51:10 -0400

    Rework new project modal to have normal inputs instead of being a form.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import formatDistance from "date-fns/formatDistance";
 
-let projects = [];
+let projectsArray = [];
 /*
 function editTitle(obj, input) {
     obj.title = input;
@@ -24,8 +24,9 @@ const toDoItem = (title) => ({
     title,
 });
 
-const project = (projectTitle) => {
+const project = (projectTitle, projectDescription) => {
     projectTitle;
+    projectDescription;
     let toDoItemList = [];
 
     function addItem(itemTitle) {
@@ -33,26 +34,68 @@ const project = (projectTitle) => {
         toDoItemList.push(newItem);
     };
 
-    return { projectTitle, toDoItems, addItem };
+    return { projectTitle, projectDescription, toDoItemList, addItem };
 };
 
-function setModalDisplay(projectsContainer) {
-    const modalContainer = document.getElementById("modal-container");
-    const closeModalButton = document.getElementById("close-button");
-    const openModalButton = document.createElement("button");
-    openModalButton.textContent = "New Project";
-    openModalButton.addEventListener("click", (e) => {
-        modalContainer.classList.add("show");
-    });
-    closeModalButton.addEventListener("click", (e) => {
-        modalContainer.classList.remove("show");
-    });
-    projectsContainer.appendChild(openModalButton);
-};
+function addProject(newProject) {
+    projectsArray.push(newProject);
+}
+
 
 const displayController = (() => {
     const projectsContainer = document.getElementById("projects-container");
 
-    setModalDisplay(projectsContainer);
+    function _updateProjectsContainer(projectsArray) {
+        for (let i = 0; i < projectsArray.length; i++) {
+            const projectElement = document.createElement("div");
+            projectElement.classList.add("project");
+
+            const titleElement = document.createElement("h3");
+            titleElement.textContent = projectsArray[i].projectTitle;
+
+            projectElement.appendChild(titleElement);
+            projectsContainer.appendChild(projectElement);
+        };
+    };
     
+    function setModalButtons() {
+        const createProjectButton = document.getElementById("create-project-button");
+        const modalContainer = document.getElementById("modal-container");
+        const closeModalButton = document.getElementById("close-button");
+        const openModalButton = document.createElement("button");
+      
+        openModalButton.textContent = "New Project";
+        openModalButton.addEventListener("click", (e) => {
+            modalContainer.classList.add("show");
+            // temporary - reset input fields
+            const formPlaceholderInputs = document.querySelectorAll("#form-placeholder input")
+            formPlaceholderInputs.forEach(inputField => inputField.value = "");
+        });
+        closeModalButton.addEventListener("click", (e) => {
+            modalContainer.classList.remove("show");
+        });
+
+        // create project button event:
+        createProjectButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            const newProjectTitle = document.getElementById("project-title").value;
+            const newProjectDescription = document.getElementById("project-description").value;
+            const newProject = project(newProjectTitle, newProjectDescription);
+            
+            addProject(newProject);
+            _updateProjectsContainer(projectsArray);
+
+            modalContainer.classList.remove("show");
+        });
+    
+        projectsContainer.appendChild(openModalButton);
+    };
+
+    return { setModalButtons };
 })();
+
+function main() {
+    displayController.setModalButtons();
+};
+
+main();
